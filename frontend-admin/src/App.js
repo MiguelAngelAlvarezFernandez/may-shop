535be5dab5fa970924d737b9b75f24f4a910eff0
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-04-26 11:00:23 +0200

    Added favicon, title and header

--- frontend-admin/src/App.js
+++ frontend-admin/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
 import AddArticle from "./components/AddArticle/AddArticle"
 
 function App() {
   return (
     <>
-     <AddArticle></AddArticle>
+      <Header></Header>
+      <AddArticle></AddArticle>
     </>
   );
 }
