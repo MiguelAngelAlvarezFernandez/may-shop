74e60695bdcad6e7e6be63d9f42c6a12c43c5e48
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-05-03 20:42:24 +0200

    Created link from AddArticle to AddMarcasAndFamilias

--- frontend-admin/src/components/Marcas/Marcas.jsx
+++ frontend-admin/src/components/Marcas/Marcas.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react"
 import { recuperarMarcas } from "../../lib/fetch.mjs";
 import Marca from "./Marca/Marca.jsx";
+import { Link, useNavigate } from "react-router-dom";
 
 function Marcas({MarcaId, setMarcaId}) {
 
+  const navigate = useNavigate()
 const [Marcas, setMarcas] = useState([])
 
 useEffect(
@@ -17,11 +19,17 @@ function manejadorChange (event) {
   setMarcaId (event.target.value)
 }
 
+function manejadorCrearMarca() {
+  navigate("/AñadirMarcasYFamilias/")
+}
+
 return (
   <>
     <select name="Marcas" value={MarcaId} onChange={manejadorChange}>
       <Marca Marcas={Marcas}></Marca>
     </select>
+    <button onClick={manejadorCrearMarca}>Crear Marca</button>
+    <Link to="/AñadirMarcasYFamilias/">Crear Marca</Link>
   </>
     );
   }
