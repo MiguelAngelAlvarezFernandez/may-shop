d2672030d1a017e140c30284295227c3f86dc529
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-05-09 20:47:15 +0200

    More changes

--- frontend-customers/src/components/Header/Header.jsx
+++ frontend-customers/src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import logoMayShop from "./logoMayShop.png"
 import styles from "./Header.module.css"
+import logoMayShop from "../Header/Imagenes/logoMayShop.png"
+import carritoCompra from "../Header/Imagenes/carritoCompra.png"
 
 function Header() {
     return (
       <header className={styles.header}>
-      <img className={styles.logoMayShop} src={logoMayShop} alt="Logo May Beauty Shop"/>
-      <h1 className={styles.title}>MAY BEAUTY SHOP</h1>
+        <img className={styles.logoMayShop} src={logoMayShop} alt="Logo May Beauty Shop"/>
+        <h1 className={styles.title}>MAY BEAUTY SHOP</h1>
+        <div className={styles.containerCarrito}>
+          <div className={styles.contadorCarrito}>0</div>
+          <img className={styles.carritoCompra} src={carritoCompra} alt="Carrito Compra"/>
+        </div>
       </header>
     );
   }
