4c4648c94023ad7d2e5c90dca5415ef094e2d187
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-05-10 20:55:23 +0200

    Created context to Carrito

--- frontend-customers/src/App.js
+++ frontend-customers/src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navigator from './components/Navigator/Navigator';
 import { BrowserRouter } from 'react-router-dom';
+import { createContext, useState } from 'react';
+
+const CarritoContext = createContext()
 
 function App() {
+
+  const  stateCarrito = useState([])
+
   return (
     <>
     <BrowserRouter>
+    <CarritoContext.Provider value={stateCarrito}>
       <Header/>
       <Navigator/>
+    </CarritoContext.Provider>
     </BrowserRouter>
     </>
   );
