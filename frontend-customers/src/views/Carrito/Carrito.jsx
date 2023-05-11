4433d9c67ddf6b6ba1570411c04a1bb8115a3235
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-05-11 20:05:12 +0200

    Carrito step 3

--- frontend-customers/src/views/Carrito/Carrito.jsx
+++ frontend-customers/src/views/Carrito/Carrito.jsx
@@ -1,7 +1,21 @@
+import { useContext, useState } from "react";
+import { CarritoContext } from "../../App";
+
 function Carrito() {
+
+  const[carrito]=useContext(CarritoContext)
+  const[totalArticulo, setTotalArticulo]=useState("")
+
     return (
       <>
-      <h3>DIOS MEDIANTE ESTA SERÁ ALGÚN DÍA LA VISTA DEL CARRITO</h3>
+      <h3>VISTA CARRITO</h3>
+      {carrito.map(articuloCarrito =>
+      <div>
+      <div>{articuloCarrito.cantidad}</div>
+      <div>{articuloCarrito.articulo.denominacion}</div>
+      <div>{articuloCarrito.articulo.precioBruto}</div>
+      </div>
+      )}
       </>
     );
   }
