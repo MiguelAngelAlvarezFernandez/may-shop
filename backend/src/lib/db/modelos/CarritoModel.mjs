46d1d491ba0a30ce9156392f779f0cb049c32b12
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-05-30 19:47:15 +0200

    Carrito model modified

--- backend/src/lib/db/modelos/CarritoModel.mjs
+++ backend/src/lib/db/modelos/CarritoModel.mjs
@@ -4,6 +4,9 @@ import db from "../db.mjs"
 const Carrito = db.define('Carrito', {
     pedidoFirme: {
         type: DataTypes.BOOLEAN
+    },
+    paymentIntentId: {
+        type: DataTypes.STRING
     }
 });
 
