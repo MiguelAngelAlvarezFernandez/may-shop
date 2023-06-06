8e27a821848c7524b1409ecc3cfe78e4311d7c28
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-06-06 17:21:02 +0200

    Fixing location to window.location

--- frontend-customers/src/config.mjs
+++ frontend-customers/src/config.mjs
@@ -1,5 +1,5 @@
-const BACKEND_URL = location.hostname === "localhost" ?  "http://localhost:8000" : "https://may-shop.onrender.com"
-const FRONTEND_URL =  location.hostname === "localhost" ? "http://localhost:3000" : "https://may-shop-clients-frontend.onrender.com/"
+const BACKEND_URL = window.location.hostname === "localhost" ?  "http://localhost:8000" : "https://may-shop.onrender.com"
+const FRONTEND_URL =  window.location.hostname === "localhost" ? "http://localhost:3000" : "https://may-shop-clients-frontend.onrender.com/"
 
 export {
     BACKEND_URL,
