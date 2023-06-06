d68d2e91acbb5d9eec82317553f5fbc77c673a77
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-06-06 17:17:52 +0200

    Switch from environment variables to location

--- frontend-customers/src/config.mjs
+++ frontend-customers/src/config.mjs
@@ -1,5 +1,5 @@
-const BACKEND_URL = process.env.BACKEND_URL ?? "http://localhost:8000"
-const FRONTEND_URL = process.env.FRONTEND_URL ?? "http://localhost:3000"
+const BACKEND_URL = location.hostname === "localhost" ?  "http://localhost:8000" : "https://may-shop.onrender.com"
+const FRONTEND_URL =  location.hostname === "localhost" ? "http://localhost:3000" : "https://may-shop-clients-frontend.onrender.com/"
 
 export {
     BACKEND_URL,
