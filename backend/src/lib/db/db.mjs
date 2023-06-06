2135db8a8dd0db1eb3fb84e47e4b0648dacf14cc
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-06-06 19:35:24 +0200

    Some changes on Db

--- backend/src/lib/db/db.mjs
+++ backend/src/lib/db/db.mjs
@@ -11,7 +11,7 @@ switch (process.env.NODE_ENV) {
     default:
         dbOptions = {
             dialect: 'sqlite',
-            storage: './database.sqlite'
+            storage: './MayShop.sqlite'
         }
         break;
 }
