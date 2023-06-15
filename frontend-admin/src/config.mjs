39402670301eb1b48f99709fd1aeee407b91fdda
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-06-15 17:32:48 +0200

    Improved config.mjs

--- frontend-admin/src/config.mjs
+++ frontend-admin/src/config.mjs
@@ -1,4 +1,7 @@
-const BACKEND_URL = window.location.hostname === "localhost" ?  "http://localhost:8000" : "https://may-shop.onrender.com"
+const BACKEND_URL = ["localhost", "127.0.0.1"].includes(window.location.hostname)
+    ? "http://localhost:8000"
+    : "https://may-shop.onrender.com"
+
 const FRONTEND_URL =  window.location.hostname === "localhost" ? "http://localhost:3000" : "https://may-shop-admin-frontend.onrender.com"
 
 export {
