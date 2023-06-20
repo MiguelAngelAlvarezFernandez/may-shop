c047b6eea8f3311c4ee08731c675cab245e3ce65
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-06-20 20:24:00 +0200

    Arreglo incidencia recuperar carrito tras formalizar pago

--- frontend-customers/src/views/ConfirmacionPago/ConfirmacionPago.jsx
+++ frontend-customers/src/views/ConfirmacionPago/ConfirmacionPago.jsx
@@ -1,8 +1,16 @@
 import Pagao from "./Pagao.gif"
 import styles from "./ConfirmacionPago.module.css"
+import { recuperarCarrito } from "../../lib/fetch.mjs";
+import { CarritoContext } from "../../App";
 
 function ConfirmacionPago() {
 
+  const[_, setCarrito]=useContext(CarritoContext)
+
+  useEffect(
+    ()=> recuperarCarrito(setCarrito)
+    ,[]
+    )
   
     return (
       <div className={styles.container}>
